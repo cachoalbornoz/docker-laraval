3a3f3c959a198787ed359f1739ccb42abd47e967
Guillermo Albornoz <cahoalbornoz@gmail.com>
2024-03-22 12:42:20 -0300

    First commit

---
+++ www/resources/views/lecturas/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('title', 'Lecturas')
+
+@section('content')
+
+    @include('errors.error')
+
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-lg-12">
+            <div class="card">
+                <div class="card-header">
+                    Lecturas
+                    <button id="stop-lecturas" class="btn btn-outline-danger btn-sm">Detener</button>
+                </div>
+
+                <div class="card-body">
+                    <div id="div-datalogger">
+                        @include('lecturas.datalogger')
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+@endsection
